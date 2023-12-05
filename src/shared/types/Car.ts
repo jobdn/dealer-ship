bac553c4d6e05418830d4e3a837238b23252b8b4
Dan Pisarev <danila.pisarev@facecast.net>
2023-12-05 21:45:03 +0300

    Добавляем таблицу для отображения автомобилей

---
+++ src/shared/types/Car.ts
@@ -0,0 +1,11 @@
+export interface Car {
+  id: number;
+  vehicle_brand: string;
+  brand_model: string;
+  vin: string;
+  year_of_manufacture: string;
+  color: string;
+  price: string;
+  creatorId: number;
+  editorId: number;
+}
