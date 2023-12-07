f29c426eef76193b6ebb7f7c1c626c910ddd3387
Никита <nkrapivin228@gmail.com>
2023-12-07 16:58:53 +0300

    сделал ссылки на регистрацию и вход

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import "./App.css";
-import {AuthForm}  from "./component/AuthForm";
+import { AuthForm } from "./component/component/AuthForm";
+import { LoginForm } from "./component/component/LoginForm";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <AuthForm />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/auth" />} />
+        <Route path="/auth" element={<AuthForm />} />
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default  App ;
+export default App;
