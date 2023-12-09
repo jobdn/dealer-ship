166339e281ff6c6d1304fac8e0010e6005747572
Dan Pisarev <danila.pisarev@facecast.net>
2023-12-09 16:31:15 +0300

    Добавляет авторизацию

--- src/pages/Cars/Cars.tsx
+++ src/pages/Cars/Cars.tsx
@@ -1,38 +1,26 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import styles from "./Cars.module.css";
 import { Car } from "../../shared/types";
 import { CarTable } from "../../components/CarTable";
-
-// Сначала нужно залогиниться, чтобы получить список авто
-const token = "";
-
-async function fetchCars(): Promise<Car[] | undefined> {
-  try {
-    const response = await fetch("http://localhost:3001/api/dealer-ship", {
-      method: "GET",
-      headers: {
-        Authorization: "Bearer " + token,
-        "Content-Type": "application/json",
-      },
-    });
-
-    const cars = (await response.json()) as Car[];
-
-    return cars;
-  } catch (error) {
-    console.log("Man it's error: ", error);
-  }
-}
+import { useLocalStorage } from "../../shared/hooks/useLocalStorage";
+import { TOKEN } from "../../shared/constants";
 
 export const Cars = () => {
   const [cars, setCars] = useState<Car[]>([]);
+  const [token] = useLocalStorage(TOKEN);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    fetchCars().then((cars) => {
-      if (cars) {
-        setCars(cars);
-      }
-    });
+    setIsLoading(true);
+    fetchCars()
+      .then((cars) => {
+        if (cars) {
+          setCars(cars);
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
   const handleDelete = useCallback(
@@ -54,21 +42,42 @@ export const Cars = () => {
     [cars]
   );
 
+  const fetchCars = useCallback(async (): Promise<Car[] | undefined> => {
+    try {
+      const response = await fetch("http://localhost:3001/api/dealer-ship", {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      });
+
+      const cars = (await response.json()) as Car[];
+
+      return cars;
+    } catch (error) {
+      console.log("Man it's error: ", error);
+    }
+  }, []);
+
   // const handleAdd = useCallback(async () => {});
 
-  return (
-    <div className={styles.carList}>
-      {cars.length ? (
-        <>
-          <div className={styles.wrapper}>
-            <h3>Список Автомобилей</h3>
-            <button>Добавить автомобиль</button>
-          </div>
-          <CarTable cars={cars} onDelete={handleDelete} />
-        </>
-      ) : (
-        <h3>В центре нет авто. Обратитесь позже</h3>
-      )}
-    </div>
-  );
+  const content = useMemo(() => {
+    if (isLoading) return <h3>Загрзка автомобилей</h3>;
+
+    if (!cars.length) return <h3>В центре нет авто. Обратитесь позже</h3>;
+
+    return (
+      <>
+        <div className={styles.wrapper}>
+          <h3>Список Автомобилей</h3>
+          <button>Добавить автомобиль</button>
+        </div>
+        <CarTable cars={cars} onDelete={handleDelete} />
+      </>
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cars, isLoading]);
+
+  return <div className={styles.carList}>{content}</div>;
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,31 @@
-import { createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import { Layout } from "./Layout";
 import { Home } from "./Home";
 import { Cars } from "./Cars";
+import { Auth } from "./Auth";
+import { ProtectedRoute } from "../components/ProtectedRoute";
+import { AuthLayout } from "./AuthLayout";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "cars", element: <Cars /> },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<AuthLayout />}>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />}></Route>
+        <Route
+          path="cars"
+          element={
+            <ProtectedRoute>
+              <Cars />
+            </ProtectedRoute>
+          }
+        ></Route>
+      </Route>
+
+      <Route path="/auth" element={<Auth />}></Route>
+    </Route>
+  )
+);
